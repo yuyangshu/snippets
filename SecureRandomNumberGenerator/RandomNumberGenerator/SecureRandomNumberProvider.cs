a5886bec289a8d89de492ad18dc9af272e40a2db
Yuyang <yuyang.shu@rokt.com>
2018-10-18 17:58:30 +1100

    RNGCryptoServiceProvider wrapper

---
+++ SecureRandomNumberGenerator/RandomNumberGenerator/SecureRandomNumberProvider.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace SecureRandom
+{
+    public class SecureRandomNumberProvider
+    {
+        private readonly RNGCryptoServiceProvider _provider;
+
+        public SecureRandomNumberProvider()
+        {
+            _provider = new RNGCryptoServiceProvider();
+        }
+
+        public sbyte Next()
+        {
+            sbyte[] randomBytes = new sbyte[1];
+
+            _provider.GetBytes((byte[])(Array)randomBytes);
+            sbyte provisionalResult = randomBytes[0];
+
+            return provisionalResult == sbyte.MinValue ? (sbyte)0 : Math.Abs(provisionalResult);
+        }
+
+        public sbyte Next(sbyte maximum)
+        {
+            sbyte upperbound = (sbyte)(sbyte.MaxValue / maximum * maximum);
+            sbyte provisionalResult;
+
+            do
+            {
+                provisionalResult = Next();
+            }
+            while (provisionalResult >= upperbound);
+
+            return (sbyte)(provisionalResult % maximum);
+        }
+
+        public sbyte Next(sbyte minimum, sbyte maximum)
+        {
+            if (minimum < 0)
+            {
+                throw new ArgumentException("Arguments must be greater than zero");
+            }
+            else if (minimum >= maximum)
+            {
+                throw new ArgumentException("The first argument must be less than the second");
+            }
+
+            return (sbyte)(minimum + Next((sbyte)(maximum - minimum)));
+        }
+
+        /// <summary>
+        /// this is endianness dependent, only works on little endian system
+        /// </summary>
+        /// <returns></returns>
+        public double NextDouble()
+        {
+            byte[] randomBytes = new byte[8];
+
+            _provider.GetBytes(randomBytes);
+            randomBytes[7] = 0b0011_1111;
+            randomBytes[6] |= 0b1111_0000;
+            return BitConverter.ToDouble(randomBytes) - 1d;
+        }
+    }
+}
---
+++ SecureRandomNumberGenerator/RandomNumberGeneratorTests/SecureRandomDistributionTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using SecureRandom;
+
+namespace SecureRandomTests
+{
+    public class SecureRandomTests
+    {
+        public static void Main()
+        {
+            SecureRandomNumberProvider _provider = new SecureRandomNumberProvider();
+
+            int[] results = new int[128];
+            int randomResult;
+            for (int i = 0; i < 10000000; i++)
+            {
+                randomResult = _provider.Next();
+                results[randomResult] += 1;
+            }
+
+            for (int i = 0; i < 128; i++)
+                Console.WriteLine(i.ToString() + ": " + results[i].ToString());
+
+            Console.WriteLine();
+
+            results = new int[10];
+
+            for (int i = 0; i < 10000000; i++)
+            {
+                randomResult = _provider.Next(10);
+                results[randomResult] += 1;
+            }
+
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(i.ToString() + ": " + results[i].ToString());
+
+            Console.WriteLine();
+
+            results = new int[20];
+
+            for (int i = 0; i < 10000000; i++)
+            {
+                randomResult = _provider.Next(10, 20);
+                results[randomResult] += 1;
+            }
+
+            for (int i = 10; i < 20; i++)
+                Console.WriteLine(i.ToString() + ": " + results[i].ToString());
+
+            Console.WriteLine();
+
+            results = new int[100];
+            double randomDoubleResult;
+
+            for (int i = 0; i < 10000000; i++)
+            {
+                randomDoubleResult = _provider.NextDouble();
+                results[(int)(randomDoubleResult * 100)] += 1;
+            }
+
+            for (int i = 0; i < 100; i++)
+                Console.WriteLine(i.ToString() + ": " + results[i].ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
